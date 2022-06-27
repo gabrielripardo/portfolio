b94301073c1e1c319954a3c98564ac764d12cd86
Gabriel Sena <gabriel.sena@smartnx.io>
2022-06-27 19:33:54 -0300

    menu com scroll funcionando ao clicar na página

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+let body = document.getElementsByTagName("body")[0]
 let header = document.getElementsByTagName("header")[0];
 let main = document.getElementsByTagName("main")[0];
 let footer = document.getElementsByTagName("footer")[0];
 let hamburgerBtn = document.getElementById("menu-hamburger");
 let menu = document.getElementById("menu");
-let navList = document.getElementById("nav-list")
+let navList = document.getElementsByClassName("nav-list")[0]
 
 function clickHamburger() {
     console.log('clicou');
@@ -15,16 +16,25 @@ function clickHamburger() {
 }
 
 function showMenu() {
+    console.log('showMenu')
     header.style.height = "100vh"
-    main.style.display = 'none'
-    footer.style.display = 'none'
+    navList.className = 'nav-list-opened'
 }
 
 function hideMenu() {
+    console.log('hideMenu')
     header.style.height = "fit-content";
-    main.style.display = 'block'
-    footer.style.display = 'block'
+    navList.className = 'nav-list'
+}
+
+function openPage() {
+    console.log("abrindo página..")
+}
+
+function goToSection() {
+    console.log("going to section..")
+    hamburgerBtn.checked = false;
+    hideMenu()
 }
 
 hamburgerBtn.addEventListener('click', clickHamburger)
-
