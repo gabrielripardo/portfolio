d27479e2103f17928b46a90bdda6f8ee6eac30d5
gabrielripardo <gts.senna@gmail.com>
2023-02-15 00:12:33 -0300

    fix padding form

--- components/Input/TextInput.jsx
+++ components/Input/TextInput.jsx
@@ -2,7 +2,7 @@ import { Slot } from "@radix-ui/react-slot";
 
 function TextInputRoot({ children }) {
   return (
-    <div className="flex items-start gap-3 h-100 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
+    <div className="flex items-start gap-3 h-100 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
       {children}
     </div>
   );
@@ -11,7 +11,7 @@ function TextInputRoot({ children }) {
 export function TextInputInput(props) {
   return (
     <input
-      className="bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+      className="py-4 px-3 bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
       {...props}
     />
   );
@@ -20,14 +20,14 @@ export function TextInputInput(props) {
 export function TextInputTextArea(props) {
   return (
     <textarea 
-      className="bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+      className="py-4 px-3 bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
       {...props}    
     />
   );
 }
 
 export function TextInputIcon({ children }) {
-  return <Slot className="w-6 h-6 text-gray-400">{children}</Slot>;
+  return <Slot className="ml-3 mt-3 w-6 h-6 text-gray-400">{children}</Slot>;
 }
 
 export const TextInput = {
